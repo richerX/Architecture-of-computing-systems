afd195d71d2a645e483d1c79e7b8b60458469df3
Ilya <26014350+richerX@users.noreply.github.com>
2021-09-23 14:45:38 +0300

    Add files via upload

---
+++ task 1/build/CMakeFiles/task.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ilya/Рабочий стол/project/animal.cpp" "CMakeFiles/task.dir/animal.cpp.o" "gcc" "CMakeFiles/task.dir/animal.cpp.o.d"
+  "/home/ilya/Рабочий стол/project/beast.cpp" "CMakeFiles/task.dir/beast.cpp.o" "gcc" "CMakeFiles/task.dir/beast.cpp.o.d"
+  "/home/ilya/Рабочий стол/project/bird.cpp" "CMakeFiles/task.dir/bird.cpp.o" "gcc" "CMakeFiles/task.dir/bird.cpp.o.d"
+  "/home/ilya/Рабочий стол/project/container.cpp" "CMakeFiles/task.dir/container.cpp.o" "gcc" "CMakeFiles/task.dir/container.cpp.o.d"
+  "/home/ilya/Рабочий стол/project/fish.cpp" "CMakeFiles/task.dir/fish.cpp.o" "gcc" "CMakeFiles/task.dir/fish.cpp.o.d"
+  "/home/ilya/Рабочий стол/project/includes.cpp" "CMakeFiles/task.dir/includes.cpp.o" "gcc" "CMakeFiles/task.dir/includes.cpp.o.d"
+  "/home/ilya/Рабочий стол/project/main.cpp" "CMakeFiles/task.dir/main.cpp.o" "gcc" "CMakeFiles/task.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
