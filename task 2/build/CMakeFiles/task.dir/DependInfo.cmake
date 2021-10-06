fc5198d1881091cae6f42b87cba52f616c38ae82
Ilya <26014350+richerX@users.noreply.github.com>
2021-10-06 17:20:51 +0300

    Add files via upload

---
+++ task 2/build/CMakeFiles/task.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ilya/Рабочий стол/HW 2/code/animal.cpp" "CMakeFiles/task.dir/animal.cpp.o" "gcc" "CMakeFiles/task.dir/animal.cpp.o.d"
+  "/home/ilya/Рабочий стол/HW 2/code/beast.cpp" "CMakeFiles/task.dir/beast.cpp.o" "gcc" "CMakeFiles/task.dir/beast.cpp.o.d"
+  "/home/ilya/Рабочий стол/HW 2/code/bird.cpp" "CMakeFiles/task.dir/bird.cpp.o" "gcc" "CMakeFiles/task.dir/bird.cpp.o.d"
+  "/home/ilya/Рабочий стол/HW 2/code/container.cpp" "CMakeFiles/task.dir/container.cpp.o" "gcc" "CMakeFiles/task.dir/container.cpp.o.d"
+  "/home/ilya/Рабочий стол/HW 2/code/fish.cpp" "CMakeFiles/task.dir/fish.cpp.o" "gcc" "CMakeFiles/task.dir/fish.cpp.o.d"
+  "/home/ilya/Рабочий стол/HW 2/code/includes.cpp" "CMakeFiles/task.dir/includes.cpp.o" "gcc" "CMakeFiles/task.dir/includes.cpp.o.d"
+  "/home/ilya/Рабочий стол/HW 2/code/main.cpp" "CMakeFiles/task.dir/main.cpp.o" "gcc" "CMakeFiles/task.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ task 2/build/CMakeFiles/task.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "../bin/task"
+  "../bin/task.pdb"
+  "CMakeFiles/task.dir/animal.cpp.o"
+  "CMakeFiles/task.dir/animal.cpp.o.d"
+  "CMakeFiles/task.dir/beast.cpp.o"
+  "CMakeFiles/task.dir/beast.cpp.o.d"
+  "CMakeFiles/task.dir/bird.cpp.o"
+  "CMakeFiles/task.dir/bird.cpp.o.d"
+  "CMakeFiles/task.dir/container.cpp.o"
+  "CMakeFiles/task.dir/container.cpp.o.d"
+  "CMakeFiles/task.dir/fish.cpp.o"
+  "CMakeFiles/task.dir/fish.cpp.o.d"
+  "CMakeFiles/task.dir/includes.cpp.o"
+  "CMakeFiles/task.dir/includes.cpp.o.d"
+  "CMakeFiles/task.dir/main.cpp.o"
+  "CMakeFiles/task.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
